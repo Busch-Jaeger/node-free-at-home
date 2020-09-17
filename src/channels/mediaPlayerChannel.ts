c6c35057dfb4e743e90457e82ecd63cc1cf18141
Thaddaeus Koehler <thaddaeus.koehler@de.abb.com>
2020-09-17 15:31:05 +0200

    Added Set PlayControls

--- src/channels/mediaPlayerChannel.ts
+++ src/channels/mediaPlayerChannel.ts
@@ -12,19 +12,27 @@ enum PlayMode {
     buffering = 3,
 }
 
-enum PlayCommand {
-    Unmute = 0,
-    Mute = 1,    
+enum PlayCommand {   
     Next = 2,
     Previous = 3,    
     VolumeDec = 4,
     VolumeInc = 12,    
 }
 
+enum SetMute {
+    Unmute = 0,
+    Mute = 1
+}
+
+enum PlayControls {
+    PlayStop = 3,
+    PlayStopNextPrev = 127
+}
+
 interface ChannelEvents {
     playModeChanged(value: PlayMode.playing | PlayMode.paused): void;
     playCommandChanged(value: PlayCommand.Next | PlayCommand.Previous | PlayCommand.VolumeDec | PlayCommand.VolumeInc): void;
-    muteChanged(value: PlayCommand.Mute | PlayCommand.Unmute): void;
+    muteChanged(value: SetMute.Mute | SetMute.Unmute): void;
     playVolumeChanged(value: number): void;
 }
 
@@ -39,10 +47,12 @@ export class MediaPlayerChannel extends Mixin(Channel, (EventEmitter as { new():
 
     public static readonly PlayMode = PlayMode;
     public static readonly PlayCommand = PlayCommand;
+    public static readonly PlayControls = PlayControls;
+    public static readonly SetMute = SetMute;
 
     protected dataPointChanged(id: PairingIds, value: string): void {
-        console.log("ID:" + id);
-        console.log("with value: " + value);
+        //console.log("ID:" + id);
+        //console.log("with value: " + value);
         switch (id) {
             case PairingIds.AL_MEDIA_PLAY: // play
                 this.emit("playModeChanged", PlayMode.playing);
@@ -65,8 +75,8 @@ export class MediaPlayerChannel extends Mixin(Channel, (EventEmitter as { new():
                 this.emit("playVolumeChanged", parseInt(value));
                 break;
             case PairingIds.AL_MEDIA_MUTE:
-                if (parseInt(value) === PlayCommand.Mute) this.emit("muteChanged", PlayCommand.Mute);
-                else if (parseInt(value) === PlayCommand.Unmute) this.emit("muteChanged", PlayCommand.Unmute);
+                if (parseInt(value) === SetMute.Mute) this.emit("muteChanged", SetMute.Mute);
+                else if (parseInt(value) === SetMute.Unmute) this.emit("muteChanged", SetMute.Unmute);
                 break;
         }
     }
@@ -85,6 +95,18 @@ export class MediaPlayerChannel extends Mixin(Channel, (EventEmitter as { new():
 
     setPlayMode(playMode: PlayMode) {
         this.setDatapoint(PairingIds.AL_PLAYBACK_STATUS, playMode.toString()); // todo: add number to PairingIds
+    }
+
+    setMute(mute: SetMute): void {
+        this.setDatapoint(PairingIds.AL_INFO_MUTE, mute.toString());
+    }
+
+    setVolume(value: number) {
+        this.setDatapoint(PairingIds.AL_INFO_ACTUAL_VOLUME, value.toString());
+    }
+    
+    setControls(value: PlayControls) {
+        this.setDatapoint(PairingIds.AL_ALLOWED_PLAYBACK_ACTIONS, value.toString());
     }
 
 }
