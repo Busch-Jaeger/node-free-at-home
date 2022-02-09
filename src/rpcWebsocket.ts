a41138510212989d2e2afe77ff4ceb8a81e92af9
Stefan Guelland <stefan.guelland@de.abb.com>
2022-02-09 19:20:19 +0100

    refactor rpc websocket

--- src/rpcWebsocket.ts
+++ src/rpcWebsocket.ts
@@ -7,40 +7,6 @@ import * as API from './rpcApi';
 
 export { JSONRPCParams };
 
-export class RpcWebsocket {
-    server: JSONRPCServer;
-    websocket: AutoReconnectWebSocket;
-    constructor(ref: string) {
-        this.server = new JSONRPCServer()
-
-        const username: string = process.env.FREEATHOME_API_USERNAME || "installer";
-        const password: string = process.env.FREEATHOME_API_PASSWORD || "12345";
-
-        const authenticationHeader = {
-            Authorization: 'Basic ' + Buffer.from(username + ':' + password).toString('base64')
-        };
-
-        this.websocket = new AutoReconnectWebSocket("http://localhost/api/rpc/v1", "/" + ref + "/websocket", {
-            ...authenticationHeader
-        });
-
-        this.websocket.on("message", async (message: string) => {
-            console.log(message);
-            const result = await this.server.receiveJSON(message);
-            this.websocket.send(JSON.stringify(result));
-        });
-    }
-
-    addMethod(name: string, method: SimpleJSONRPCMethod): void {
-        this.server.addMethod(name, method)
-    }
-
-    close() {
-        this.websocket.close();
-    }
-
-}
-
 const baseUrl = process.env.FREEATHOME_RPC_API_BASE_URL
     ?? (process.env.FREEATHOME_BASE_URL) ? process.env.FREEATHOME_BASE_URL + "/api/rpc/v1" : "http://localhost";
 const username = process.env.FREEATHOME_API_USERNAME ?? "installer";
@@ -61,6 +27,39 @@ const connectionOptions = {
     baseUrl: baseUrl,
     fetch: fetch,
     agent: (useUnixSocket) ? unixSocketAgent : http.globalAgent
+}
+export class RpcWebsocket {
+    server: JSONRPCServer;
+    websocket: AutoReconnectWebSocket;
+    constructor(ref: string) {
+        this.server = new JSONRPCServer()
+
+        const username: string = process.env.FREEATHOME_API_USERNAME || "installer";
+        const password: string = process.env.FREEATHOME_API_PASSWORD || "12345";
+
+        const authenticationHeader = {
+            Authorization: 'Basic ' + Buffer.from(username + ':' + password).toString('base64')
+        };
+
+        this.websocket = new AutoReconnectWebSocket(baseUrl, "/" + ref + "/websocket", {
+            ...authenticationHeader
+        });
+
+        this.websocket.on("message", async (message: string) => {
+            console.log(message);
+            const result = await this.server.receiveJSON(message);
+            this.websocket.send(JSON.stringify(result));
+        });
+    }
+
+    addMethod(name: string, method: SimpleJSONRPCMethod): void {
+        this.server.addMethod(name, method)
+    }
+
+    close() {
+        this.websocket.close();
+    }
+
 }
 
 export function UploadAuxiliaryDeviceData(reference: string, body?: string | undefined) {
