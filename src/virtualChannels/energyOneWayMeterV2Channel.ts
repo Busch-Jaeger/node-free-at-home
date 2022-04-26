fdd96f5702b83fef44810d3e505bc207e719e127
Tobias Bräutigam <braeutigam@gonicus.de>
2022-04-26 11:02:10 +0200

    use changed pid names

--- src/virtualChannels/energyOneWayMeterV2Channel.ts
+++ src/virtualChannels/energyOneWayMeterV2Channel.ts
@@ -23,14 +23,14 @@ export class EnergyOneWayMeterV2Channel extends Channel {
     /**
      * @param value {String} unit Wh (DPT_ACTIVE_ENERGY)
      */
-    public setConsumedEnergyToday(value: string): Promise<void> {
-        return this.setDatapoint(PairingIds.AL_MEASURED_CONSUMED_ENERGY_TODAY, value);
+    public setExportedEnergyToday(value: string): Promise<void> {
+        return this.setDatapoint(PairingIds.AL_MEASURED_EXPORTED_ENERGY_TODAY, value);
     }
     
     /**
     * @param value {String} unit kWh (DPT_ACTIVE_ENERGY_KWH)
     */
-    public setTotalEnergyConsumed(value: string): Promise<void> {
-        return this.setDatapoint(PairingIds.AL_MEASURED_TOTAL_ENERGY_CONSUMED, value);
+    public setTotalEnergyExported(value: string): Promise<void> {
+        return this.setDatapoint(PairingIds.AL_MEASURED_TOTAL_ENERGY_EXPORTED, value);
     }
 }
--- src/virtualChannels/energyTwoWayMeterV2Channel.ts
+++ src/virtualChannels/energyTwoWayMeterV2Channel.ts
@@ -22,28 +22,28 @@ export class EnergyTwoWayMeterV2Channel extends Channel {
     /**
      * @param value {String} unit Wh (DPT_ACTIVE_ENERGY)
      */
-    public setConsumedEnergyToday(value: string): Promise<void> {
-        return this.setDatapoint(PairingIds.AL_MEASURED_CONSUMED_ENERGY_TODAY, value);
+    public setImportedEnergyToday(value: string): Promise<void> {
+        return this.setDatapoint(PairingIds.AL_MEASURED_IMPORTED_ENERGY_TODAY, value);
     }
     
     /**
      * @param value {String} unit Wh (DPT_ACTIVE_ENERGY)
      */
-    public setProvidedEnergyToday(value: string): Promise<void> {
-        return this.setDatapoint(PairingIds.AL_MEASURED_PROVIDED_ENERGY_TODAY, value);
+    public setExportedEnergyToday(value: string): Promise<void> {
+        return this.setDatapoint(PairingIds.AL_MEASURED_EXPORTED_ENERGY_TODAY, value);
     }
 
     /**
     * @param value {String} unit kWh (DPT_ACTIVE_ENERGY_KWH)
     */
-    public setTotalEnergyConsumed(value: string): Promise<void> {
-        return this.setDatapoint(PairingIds.AL_MEASURED_TOTAL_ENERGY_CONSUMED, value);
+    public setTotalEnergyImported(value: string): Promise<void> {
+        return this.setDatapoint(PairingIds.AL_MEASURED_TOTAL_ENERGY_IMPORTED, value);
     }
 
     /**
     * @param value {String} unit kWh (DPT_ACTIVE_ENERGY_KWH)
     */
-    public setTotalEnergyProvided(value: string): Promise<void> {
-        return this.setDatapoint(PairingIds.AL_MEASURED_TOTAL_ENERGY_PROVIDED, value);
+    public setTotalEnergyExported(value: string): Promise<void> {
+        return this.setDatapoint(PairingIds.AL_MEASURED_TOTAL_ENERGY_EXPORTED, value);
     }
 }
