6c7fd18f8db182b7669e23efdb55a459417769a9
Stefan Guelland <stefan.guelland@de.abb.com>
2024-04-05 13:13:24 +0000

    name is not manadory on virtual devices

--- src/freeAtHomeApi.ts
+++ src/freeAtHomeApi.ts
@@ -258,7 +258,7 @@ export class FreeAtHomeApi extends (EventEmitter as { new(): Emitter }) {
         }
     }
 
-    async createDevice(deviceType: API.VirtualDeviceType, nativeId: string, displayName: string, flavor?: string, capabilities?: Capabilities[]): Promise<ApiVirtualDevice> {
+    async createDevice(deviceType: API.VirtualDeviceType, nativeId: string, displayName?: string, flavor?: string, capabilities?: Capabilities[]): Promise<ApiVirtualDevice> {
         if(false === nativeIdRegExp.test(nativeId))
             throw new Error("nativeId contains not supported characters");
         try {
